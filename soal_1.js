e1a022ac92f34579cb1a45e4c06f9e64f8e8bda6
hansmalvin <hansmalvin7@gmail.com>
2024-03-07 14:50:37 +0700

    commit
    Hans Malvin Djojo - 535230083

---
+++ soal_1.js
@@ -0,0 +1,14 @@
+console.log("Bilangan prima dari angka 1 hingga 1000 adalah: ")
+
+for(let num = 2; num <= 1000; num++){
+    let bilprima = true
+    for(let i = 2; i < num; i++){
+        if(num % i === 0){
+            bilprima = false
+            break
+        }
+    }
+    if(bilprima){
+        console.log(num + " ")
+    }
+}
